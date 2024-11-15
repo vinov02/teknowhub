0692372927e127c27cfdb020cf08c49a97ef341d
vinov02 <vinoviji676@gmail.com>
2024-11-15 16:46:36 +0530

    initial commit

---
+++ client/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Add any JavaScript code if needed
+});
+document.querySelector('.whatsapp-float').addEventListener('click', function() {
+    // You can add custom behavior here if needed
+    console.log('WhatsApp icon clicked');
+});
